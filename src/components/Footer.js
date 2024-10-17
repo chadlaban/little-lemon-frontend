16a3e29228828fab270273993f15e61d8e0966df
chadlaban <laban.chad@gmail.com>
2024-10-17 14:22:17 +0800

    Add: initial layout and components.

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div className="main-grid">
+      <h1>This is the Footer</h1>{" "}
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <div className="main-grid">
+      <h1>This is the Header</h1>{" "}
+    </div>
+  );
+}
+
+export default Header;
